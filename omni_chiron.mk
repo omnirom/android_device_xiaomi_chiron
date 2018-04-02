59af9c54964869d7fc41e04d2da3f2807417e0d0
andr68rus <ashtmb@gmail.com>
2018-04-02 13:36:26 +0300

    signature: update fingerprint to 8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA
    Change-Id: Ic93984b1f5bb9fb447838dee74c03ddd2d443d5b

--- omni_chiron.mk
+++ omni_chiron.mk
@@ -47,8 +47,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="chiron" \
     PRODUCT_NAME="chiron" \
-    BUILD_FINGERPRINT="Xiaomi/chiron/chiron:7.1.1/NMF26X/V8.5.3.0.NDECNEF:user/release-keys" \
-    PRIVATE_BUILD_DESC="chiron-user 7.1.1 NMF26X V8.5.3.0.NDECNEF release-keys"
+    BUILD_FINGERPRINT="Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys" \
+    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
